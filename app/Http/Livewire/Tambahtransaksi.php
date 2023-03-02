f035072cf5c8569b8c12812eda4f72ff5294b978
xeizer <keiser.form@gmail.com>
2023-03-02 08:19:59 +0700

    first commit

---
+++ app/Http/Livewire/Tambahtransaksi.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class Tambahtransaksi extends Component
+{
+    public function render()
+    {
+        return view('livewire.tambahtransaksi');
+    }
+}
